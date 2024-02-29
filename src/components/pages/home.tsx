da431bb3377d16823d504e5f5deb8498899f4e07
Serge Mezui <68657194+SergeMezui16@users.noreply.github.com>
2024-02-29 13:38:04 +0100

    Add: reduce home page image size

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -13,7 +13,7 @@ export const HomeCard = () => {
       <div className='flex flex-col tablet:flex-row'>
         <MotionDiv className='w-full items-end tablet:items-center justify-center flex'>
           <Image
-            className='w-[184px] h-[184px] tablet:w-[344px] tablet:h-[344px] rounded-full shadow-lg shadow-primary mb-8 tablet:mb-0 transition-all hover:scale-105'
+            className='w-[124px] h-[124px] md:w-[184px] md:h-[184px] tablet:w-[344px] tablet:h-[344px] rounded-full shadow-lg shadow-primary mb-8 tablet:mb-0 transition-all hover:scale-105'
             src={photo}
             alt='Logo Serge Mezui'
             width={314}
@@ -21,11 +21,12 @@ export const HomeCard = () => {
           />
         </MotionDiv>
         <MotionDiv className='w-full tablet:text-end flex flex-col tablet:gap-3 gap-6 text-center'>
-          <h1 className='tablet:text-8xl text-6xl font-bold'>
+          <h1 className='tablet:text-8xl md:text-6xl text-3xl font-bold'>
             Serge Mezui<span className='text-primary'>.</span>
           </h1>
-          <p className='text-xl text-secondary-foreground'>
-            Backend Developper. I am a web enthusiast who develops solutions to make your life easier and improve your experience through the web.
+          <p className='text-lg md:text-xl text-secondary-foreground'>
+            Backend Developper. I am a web enthusiast who develops solutions to
+            make your life easier and improve your experience through the web.
           </p>
           <p className='text-muted-foreground'>the Freak.</p>
           <Link href='/#service'>
