30faf0c78c2fd71f3bb11711ad215ab16be93d38
Serge Mezui <68657194+SergeMezui16@users.noreply.github.com>
2024-02-29 13:46:16 +0100

    Add: increase the menu icon size

--- src/components/molecule/menu.tsx
+++ src/components/molecule/menu.tsx
@@ -11,21 +11,20 @@ const links = [
 
 export const Menu = ({ onClick }: { onClick?: () => void }) => {
   return (
-      <ul className='flex flex-col tablet:flex-row gap-8 text-4xl tablet:text-2xl items-center'>
-        {links.map((link) => (
-          <li
-            onClick={onClick}
-            className='group cursor-pointer'
-            key={link.href}
-          >
-            <MotionSpan className='group cursor-pointer flex flex-col'>
-              <Link className='font-euro-stile font-thin' href={link.href}>
-                {link.label}
-              </Link>
-              <span className='hidden border-white bg-primary tablet:inline w-0 h-2 opacity-0 border border-primary group-hover:w-3/4 group-hover:opacity-100 transition-all'></span>
-            </MotionSpan>
-          </li>
-        ))}
-      </ul>
+    <ul className='flex flex-col tablet:flex-row gap-8 text-4xl tablet:text-2xl items-center'>
+      {links.map((link) => (
+        <li onClick={onClick} className='group cursor-pointer' key={link.href}>
+          <MotionSpan className='group cursor-pointer flex flex-col'>
+            <Link
+              className='font-euro-stile font-thin text-2xl'
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+            <span className='hidden border-white bg-primary tablet:inline w-0 h-2 opacity-0 border border-primary group-hover:w-3/4 group-hover:opacity-100 transition-all'></span>
+          </MotionSpan>
+        </li>
+      ))}
+    </ul>
   );
 };
