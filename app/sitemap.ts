f7a8c7e5eb7f9370cbbb8a10268464918830ce01
Serge Mezui <68657194+SergeMezui16@users.noreply.github.com>
2024-03-04 22:37:18 +0100

    Add: sitemap auto generated

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,5 +9,35 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 1,
     },
+    {
+      url: SITE_URL + '/#home',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: SITE_URL + '/#service',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: SITE_URL + '/#skill',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: SITE_URL + '/#project',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: SITE_URL + '/#contact',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.4,
+    },
   ];
 }
