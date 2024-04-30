3d49e82c2b3edcf08dd9f18f09d713bdad4d3a8b
Serge Mezui <68657194+SergeMezui16@users.noreply.github.com>
2024-04-30 23:16:20 +0200

    Add: serge mezui component

---
+++ src/components/atom/serge-mezui.tsx
@@ -0,0 +1,5 @@
+export const SergeMezui = () => (
+    <h4 className='text-xl tablet:text-2xl font-bold'>
+        Serge Mezui<span className='text-primary'>.</span><span className='text-sm'>dev</span>
+    </h4>
+)
