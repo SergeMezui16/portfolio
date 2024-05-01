baa9f174cf387dffe94ee621b5615e316cae37e9
Serge Mezui <68657194+SergeMezui16@users.noreply.github.com>
2024-05-01 22:04:50 +0100

    Add: sergemezui comp to footer

--- src/components/molecule/footer.tsx
+++ src/components/molecule/footer.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import pasker from '@/assets/img/pasker.png';
-import { MotionDiv } from "../atom";
+import {MotionDiv, SergeMezui} from "../atom";
 
 export const Footer = () => {
   return (
     <div className='h-1/2 pb-20 bg-card'>
       <MotionDiv className='container p-10 flex flex-col gap-4 tablet:flex-row justify-between items-center'>
-        <h1 className='text-2xl font-bold'>
-          Serge Mezui<span className='text-primary'>.</span>
-        </h1>
+        <SergeMezui />
         <div className='font-mono flex items-center gap-2'>
           <span>Powered By</span>
           <Image src={pasker} alt='Pasker Logo' className='w-32' />
