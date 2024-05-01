7833ecbb0c4b6c22b80fb33cb6ee6fbac82bb89b
Serge Mezui <68657194+SergeMezui16@users.noreply.github.com>
2024-05-01 22:04:34 +0100

    Add: color to .dev

--- src/components/atom/serge-mezui.tsx
+++ src/components/atom/serge-mezui.tsx
@@ -1,5 +1,5 @@
 export const SergeMezui = () => (
     <h4 className='text-xl tablet:text-2xl font-bold'>
-        Serge Mezui<span className='text-primary'>.</span><span className='text-sm'>dev</span>
+        Serge Mezui<span className='text-primary text-sm'>.dev</span>
     </h4>
 )
