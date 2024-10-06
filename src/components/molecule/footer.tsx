cd33dcc6a0ae9390339e82a6e296305f1b5d19ad
sergemezui <sergemezui.dev@gmail.com>
2024-10-06 22:45:40 +0100

    footer translation

--- src/components/molecule/footer.tsx
+++ src/components/molecule/footer.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import pasker from '@/assets/img/pasker.png';
 import {MotionDiv, SergeMezui} from "../atom";
+import { getScopedI18n } from "@/locales/server";
 
-export const Footer = () => {
+export const Footer = async () => {
+  const t = await getScopedI18n("footer")
   return (
     <div className='h-1/2 pb-20 bg-card'>
       <MotionDiv className='container p-10 flex flex-col gap-4 tablet:flex-row justify-between items-center'>
         <SergeMezui />
         <div className='font-mono flex items-center gap-2'>
-          <span>Powered By</span>
+          <span>{t("powered")}</span>
           <Image src={pasker} alt='Pasker Logo' className='w-32' />
         </div>
       </MotionDiv>
