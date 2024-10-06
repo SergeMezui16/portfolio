e442361eb648c52f66259261c5db2052a754fd89
sergemezui <sergemezui.dev@gmail.com>
2024-10-06 20:02:07 +0100

    improve list

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,26 +4,26 @@ import {
   HomeCard,
   ProjectCard,
   Servicecard,
-  SkillCard,
-} from '@/components/pages';
-import { Header, Social, Footer } from '@/components/molecule';
+  SkillCard
+} from "@/components/pages";
+import { Header, Social, Footer } from "@/components/molecule";
 
 export default function Home() {
   return (
     <>
-      <div className='min-h-screen'>
-        <Header />
-        <div className=''>
-          <HomeCard />
-          <Servicecard />
-          <SkillCard />
-          <ProjectCard />
-          <EnterpriseCard />
-          <ContactCard />
-          <Footer />
+      <div className="min-h-screen">
+        <Header/>
+        <div className="">
+          <HomeCard/>
+          <Servicecard/>
+          <SkillCard/>
+          <ProjectCard/>
+          <EnterpriseCard/>
+          <ContactCard/>
+          <Footer/>
         </div>
       </div>
-      <Social />
+      <Social/>
     </>
   );
 }
