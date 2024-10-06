c3a5ccf202c2a0bae2f9b927d87b18a48ba955ad
sergemezui <sergemezui.dev@gmail.com>
2024-10-06 22:43:15 +0100

    add fr translation file

---
+++ locales/lang/fr.ts
@@ -0,0 +1,120 @@
+export default {
+  manifest: {
+    name: "Serge Mezui, Développeur Web.",
+    short_name: "Serge Mezui, Développeur Web.",
+    description: "Je suis un jeune étudiant passionné par l'informatique et plus particulièrement par le développement web. Je développe des solutions qui permettent de résoudre des problèmes pour les gens du monde entier et de simplifier leur vie quotidienne grâce à l'internet. J'aime les films, les jeux vidéo et la course à pied.",
+  },
+  nav: {
+    home: 'Accueil',
+    service: 'Service',
+    skill: 'Compétence',
+    contact: 'Contact',
+    project: 'Projet',
+  },
+  home: {
+    name: "Serge Mezui",
+    bio: "Développeur Full Stack. Je suis un passionné du web qui développe des solutions pour vous faciliter la vie et améliorer votre expérience sur le web.",
+    nickName: "the Freak.",
+    next: "Voir plus"
+  },
+  service: {
+    title: "Mes services",
+      question: "Comment pouvez-vous m'aider ?",
+    description: "Je suis un jeune étudiant passionné par l'informatique et plus particulièrement par le développement web. Je développe des solutions qui permettent de résoudre des problèmes pour les gens du monde entier et de simplifier leur vie quotidienne grâce à l'internet. J'aime les films, les jeux vidéo et la course à pied.",
+    services: {
+      design: {
+        title: "Conception d'un site web personnalisé",
+        description: "Créez vos propres sites personnalisés comme celui-ci pour vous faire un nom sur le marché du web."
+      },
+      website: {
+        title: "Conception d'applications web",
+        description: "Créons des applications web pour faciliter les tâches quotidiennes."
+      },
+      api: {
+        title: "Conception d'API REST",
+        description: "Faites concevoir vos API pour vous permettre de centraliser la logique commerciale de vos projets et de gérer vos bases de données."
+      },
+      site: {
+        title: "Conception de maquettes",
+        description: "Visualisez le résultat final de vos sites web ou applications grâce à des maquettes simples et interactives."
+      },
+      showcase: {
+        title: "Création de site vitrine",
+        description: "Faites créer un site vitrine qui vous permettra de présenter à tous vos produits, réalisations, etc."
+      },
+      team: {
+        title: "Travailler au sein d'une équipe de développement de projets",
+        description: "Invitez-moi à participer à un projet de développement web."
+      }
+    }
+  },
+  skill: {
+    title: "Mes Compétences",
+    question: "Qu'est-ce que vous en savez faire ?",
+    description: "Je suis un développeur autonome, créatif et professionnel. Passionné par le travail back-end (administration de bases de données, conception d'API, administration de serveurs...), je peux également prendre en charge des tâches front-end (intégration web, ...) ou la construction de modèles.",
+    techno: "Technos",
+    skills: {
+      design: {
+        title: "Design",
+        description: "Je réalise des maquettes interactives de sites web pour visualiser l'aspect des sites finaux."
+      },
+      backend: {
+        title: "Back-end",
+        description: "Je construis des API et des sites web avec des technologies back-end et/ou je crée des bases de données, des systèmes d'authentification et des applications côté serveur."
+      },
+      frontend: {
+        title: "Front-end",
+        description: "Je crée des applications de type SPA (Single Page Application) ou des applications rapides et interactives."
+      },
+      others: {
+        title: "Autres",
+        description: "Je sais aussi utiliser des outils qui m'aident à mieux faire mon travail. J'utilise des technologies communes à plusieurs types de projets pour m'adapter aux normes."
+      }
+    }
+  },
+  project: {
+    title: "Mes Projets",
+    question: "Qu'avez-vous déjà fait ?",
+    visit: "Visiter",
+    projects: {
+      transitup: {
+        name: "Transit'Up",
+        description: "Solution numérique pour coter et faire transiter les colis dans le monde entier."
+      },
+      spacecoder: {
+        name: "SpaceCoder",
+        description: "Partager des conseils et des outils pour mieux s'imprégner de l'outil numérique."
+      },
+      servant: {
+        name: "Servant",
+        description: "Projet de recensement indépendant des servants de messe au Cameroun."
+      },
+      spacecoderapi: {
+        name: "SpaceCoder API",
+        description: "API de SpaceCoder V3."
+      },
+      akomaserver: {
+        name: "Akoma Server",
+        description: "Logiciel permettant de gérer la communication, le partage et le stockage de données au sein de l'entreprise de manière sécurisée et synchronisée."
+      }
+    }
+  },
+  enterprise: {
+    title: "Ils m'ont fait confiance",
+    question: "Avec qui avez-vous travaillé ?"
+  },
+  contact: {
+    title: "Contacts",
+    question: "Comment puis-je vous trouver ?",
+    phone: "Téléphone",
+    email: "Adresse email",
+    location: "Localisation",
+    country: "Pays d'origine",
+    from: "Du",
+    resume: "Mon CV",
+    open_resume: "cliquez pour voir le CV",
+  },
+  footer: {
+    powered: "Propulsé par",
+  }
+} as const;
