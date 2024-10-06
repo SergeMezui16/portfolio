63462726853d9153c85df45fb3e4da310463e949
sergemezui <sergemezui.dev@gmail.com>
2024-10-06 22:44:19 +0100

    bind provider

--- src/context/app-provider.tsx
+++ src/context/app-provider.tsx
@@ -1,15 +1,18 @@
-import { PropsWithChildren } from 'react';
-import { ThemeProvider } from './theme-provider';
+import { PropsWithChildren } from "react";
+import { ThemeProvider } from "./theme-provider";
+import { I18nProviderClient } from "../../locales/client";
 
-export const AppProvider = ({ children }: PropsWithChildren) => {
+export const AppProvider = ({children, locale}: PropsWithChildren<{ locale: string }>) => {
   return (
-    <ThemeProvider
-      attribute='class'
-      defaultTheme='system'
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <I18nProviderClient locale={locale}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </I18nProviderClient>
   );
 };
