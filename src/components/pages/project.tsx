5e24673750927c1b984b720a76aba57696de604b
sergemezui <sergemezui.dev@gmail.com>
2024-10-06 22:45:36 +0100

    project translation

--- src/components/pages/project.tsx
+++ src/components/pages/project.tsx
@@ -4,54 +4,56 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { ExternalLinkIcon } from 'lucide-react';
 import { MotionDiv } from '../atom';
+import { getScopedI18n } from "@/locales/server";
 
-const data = [
-  {
-    name: "Transit'Up",
-    description:
-      'Digital solution to quote and transit parcels around the world',
-    technos: ['PHP', 'Javascript', 'SQL'],
-    link: 'https://transit-up.com',
-    image: '/images/transit-up.png',
-  },
-  {
-    name: 'SpaceCoder',
-    description:
-      'Share tips and tools to better immerse yourself in digital tool',
-    link: 'https://fr.spacecoder.fun',
-    technos: ['PHP', 'Javascript', 'SQL'],
-    image: '/images/spacecoder.png',
-  },
-  {
-    name: 'Servant',
-    description: 'Cameroon independent mass workers census project',
-    link: 'https://fr.spacecoder.fun/servant',
-    technos: ['Symfony', 'Tailwind', 'SQL'],
-    image: '/images/servant.png',
-  },
-  {
-    name: 'SpaceCoder API',
-    description: "SpaceCoder's V3 Application Programming Interface",
-    technos: ['PHP', 'Open API', 'Swagger UI'],
-    link: 'https://fr.spacecoder.fun/api',
-    image: '/images/spacecoder-api.png',
-  },
-  {
-    name: 'Akoma Server',
-    description:
-      'Software for managing corporate communication, data sharing and storage in a secure, synchronized way',
-    technos: ['TypeScript', 'Next JS', 'PHP', 'SQL', 'React'],
-    image: '/images/akoma-server.png',
-  },
-] satisfies {
-  name: string;
-  description: string;
-  link?: string;
-  technos: string[];
-  image: string;
-}[];
 
-export const ProjectCard = () => {
+export const ProjectCard = async () => {
+  const t = await getScopedI18n("project");
+
+  const data = [
+    {
+      name: t("projects.transitup.name"),
+      description: t("projects.transitup.description"),
+      technos: ['PHP', 'Javascript', 'SQL'],
+      link: 'https://transit-up.com',
+      image: '/images/transit-up.png',
+    },
+    {
+      name: t("projects.spacecoder.name"),
+      description: t("projects.spacecoder.description"),
+      link: 'https://fr.spacecoder.fun',
+      technos: ['PHP', 'Javascript', 'SQL'],
+      image: '/images/spacecoder.png',
+    },
+    {
+      name: t("projects.servant.name"),
+      description: t("projects.servant.description"),
+      link: 'https://fr.spacecoder.fun/servant',
+      technos: ['Symfony', 'Tailwind', 'SQL'],
+      image: '/images/servant.png',
+    },
+    {
+      name: t("projects.spacecoderapi.name"),
+      description: t("projects.spacecoderapi.description"),
+      technos: ['PHP', 'Open API', 'Swagger UI'],
+      link: 'https://fr.spacecoder.fun/api',
+      image: '/images/spacecoder-api.png',
+    },
+    {
+      name: t("projects.akomaserver.name"),
+      description: t("projects.akomaserver.description"),
+      technos: ['TypeScript', 'Next JS', 'PHP', 'SQL', 'React'],
+      image: '/images/akoma-server.png',
+    },
+  ] satisfies {
+    name: string;
+    description: string;
+    link?: string;
+    technos: string[];
+    image: string;
+  }[];
+
+
   return (
     <div
       className='min-h-screen flex items-center bg-primary py-10'
@@ -60,10 +62,10 @@ export const ProjectCard = () => {
       <div className='container'>
         <MotionDiv className='my-10 text-center tablet:text-start'>
           <h1 className='text-6xl font-bold'>
-            My Projects<span className='text-primary-foreground'>.</span>
+            {t("title")}<span className='text-primary-foreground'>.</span>
           </h1>
           <p className='my-1 italic text-primary-foreground'>
-            What did you already done?
+            {t("question")}
           </p>
         </MotionDiv>
         <div className=''>
@@ -92,7 +94,7 @@ export const ProjectCard = () => {
                   {project.link && (
                     <Link href={project.link} target='_blank'>
                       <Button className='rounded-full'>
-                        Visit {project.name}{' '}
+                        {t("visit")} {project.name}{' '}
                         <ExternalLinkIcon className='w-3 h-3 stroke-1' />
                       </Button>
                     </Link>
