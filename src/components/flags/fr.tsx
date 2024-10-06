09bc1995ce9abea433cacb816321a1090965c290
sergemezui <sergemezui.dev@gmail.com>
2024-10-07 00:04:12 +0100

    add flags

---
+++ src/components/flags/fr.tsx
@@ -0,0 +1,14 @@
+import { IconProps } from "@/components/icons";
+
+export const FlagFr = (props: IconProps) => (
+  <svg {...props} viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+       aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet"
+       fill="#000000">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <path fill="#ED2939" d="M36 27a4 4 0 0 1-4 4h-8V5h8a4 4 0 0 1 4 4v18z"></path>
+      <path fill="#002495" d="M4 5a4 4 0 0 0-4 4v18a4 4 0 0 0 4 4h8V5H4z"></path>
+      <path fill="#EEE" d="M12 5h12v26H12z"></path>
+    </g>
+  </svg>);
---
+++ src/components/flags/ga.tsx
@@ -0,0 +1,14 @@
+import { IconProps } from "@/components/icons";
+
+export const FlagGa = (props: IconProps) => (
+  <svg viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+       aria-hidden="true" role="img" {...props} preserveAspectRatio="xMidYMid meet"
+       fill="#000000">
+    <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+    <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+    <g id="SVGRepo_iconCarrier">
+      <path fill="#009E60" d="M32 5H4a4 4 0 0 0-4 4v5h36V9a4 4 0 0 0-4-4z"></path>
+      <path fill="#3A75C4" d="M0 27a4 4 0 0 0 4 4h28a4 4 0 0 0 4-4v-5H0v5z"></path>
+      <path fill="#FCD116" d="M0 14h36v8H0z"></path>
+    </g>
+  </svg>);
