59ccc31aaf60e2a9c8104ea32a8d3954f9246f3c
sergemezui <sergemezui.dev@gmail.com>
2024-10-06 22:43:20 +0100

    add en translation file

---
+++ locales/lang/en.ts
@@ -0,0 +1,121 @@
+
+export default {
+  manifest: {
+    name: "Serge Mezui, Web Developer.",
+    short_name: "Serge Mezui, Web Developer.",
+    description: "I'm a young student with a passion for computer science, more specifically web development. I develop solutions that solve problems for people all over the world and simplify their everyday life experience through the internet. I like movies, video games and running.",
+  },
+  nav: {
+    home: "Home",
+    service: "Service",
+    skill: "Skills",
+    contact: "Contact",
+    project: "Project"
+  },
+  home: {
+    name: "Serge Mezui",
+    bio: "Full Stack Developer. I am a web enthusiast who develops solutions to make your life easier and improve your experience through the web.",
+    nickName: "the Freak.",
+    next: "See more"
+  },
+  service: {
+    title: "My Services",
+    question: "How can you help me?",
+    description: "I'm a young student with a passion for computer science, more specifically web development. I develop solutions that solve problems for people all over the world and simplify their everyday life experience through the internet. I like movies, video games and running.",
+    services: {
+      design: {
+        title: "Personalized website design",
+        description: "Get your own customized sites like this one to make a name for yourself on the web market."
+      },
+      website: {
+        title: "Web App Design",
+        description: "Let us create web applications to make everyday tasks easier."
+      },
+      api: {
+        title: "REST API design",
+        description: "Have your APIs designed to enable you to centralize the business logic of your projects and manage your databases."
+      },
+      site: {
+        title: "Site design",
+        description: "Visualize the final result of your web sites or applications through simple, interactive mock-ups."
+      },
+      showcase: {
+        title: "Creation of a showcase site",
+        description: "Have a showcase site created to enable you to present your products, achievements, etc. to everyone."
+      },
+      team: {
+        title: "Working in a project development team",
+        description: "Invite me to a web development project."
+      }
+    }
+  },
+  skill: {
+    title: "My Skills",
+    question: "What do you know?",
+    description: "I'm an autonomous, creative and professional developer. Passionate about backend work (database administration, API design, server administration...) I can also take on front-end tasks (web integration, ...) or model building.",
+    techno: "Technos",
+    skills: {
+      design: {
+        title: "Design",
+        description: "I make interactive website mock-ups to visualize how the final websites will look."
+      },
+      backend: {
+        title: "Backend",
+        description: "I build APIs and websites with back-end technologies and/or create databases, authentication systems and server-side applications."
+      },
+      frontend: {
+        title: "Frontend",
+        description: "I set up SPA (Single Page Application) type applications or fast, interactive applications."
+      },
+      others: {
+        title: "Others",
+        description: "I also know how to use tools that help me do my job better. I use technologies common to several types of project to adapt to standards."
+      }
+    }
+  },
+  project: {
+    title: "My Projects",
+    question: "What did you already done?",
+    visit: "Visit",
+    projects: {
+      transitup: {
+        name: "Transit'Up",
+        description: "Digital solution to quote and transit parcels around the world."
+      },
+      spacecoder: {
+        name: "SpaceCoder",
+        description: "Share tips and tools to better immerse yourself in digital tool."
+      },
+      servant: {
+        name: "Servant",
+        description: "Cameroon independent mass workers census project."
+      },
+      spacecoderapi: {
+        name: "SpaceCoder API",
+        description: "SpaceCoder's V3 Application Programming Interface."
+      },
+      akomaserver: {
+        name: "Akoma Server",
+        description: "Software for managing corporate communication, data sharing and storage in a secure, synchronized way."
+      }
+    }
+  },
+  enterprise: {
+    title: "They trusted me",
+    question: "Who have you worked with?"
+  },
+  contact: {
+    title: "Contacts",
+    question: "How can I find you?",
+    phone: "Phone",
+    email: "Email",
+    location: "Location",
+    country: "Home Country",
+    from: "From",
+    resume: "My Resume",
+    open_resume: "click to open resume",
+  },
+  footer: {
+    powered: "Powered By",
+  }
+} as const;
