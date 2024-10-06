bd35f5e258cbce505643f86d3d84924588976d4a
sergemezui <sergemezui.dev@gmail.com>
2024-10-06 20:01:21 +0100

    set resume constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,13 @@
-export const SITE_URL = 'https://sergemezui.com';
+export const SITE_URL = "https://sergemezui.com";
+
+export const DEFAULT_LANG = "en-US";
+
+/**
+ * Resume file name in English
+ */
+export const RESUME_FILE_NAME_EN = "serge-mezui-fullstack-dev-resume-en.pdf";
+
+/**
+ * Resume file name in French
+ */
+export const RESUME_FILE_NAME_FR = "serge-mezui-fullstack-dev-resume-fr.pdf";
