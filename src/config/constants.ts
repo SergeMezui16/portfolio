4ec8d5829f89c3995ec9cdef96ef432ceb5c0e5d
sergemezui <sergemezui.dev@gmail.com>
2025-06-22 14:58:01 +0100

    Add Wikipedia links for Gabon in English and French
    Defined constants for Gabon's Wikipedia URLs in English and French to improve reusability and maintainability.

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -11,3 +11,8 @@ export const RESUME_FILE_NAME_EN = "serge-mezui-fullstack-dev-resume-en.pdf";
  * Resume file name in French
  */
 export const RESUME_FILE_NAME_FR = "serge-mezui-fullstack-dev-resume-fr.pdf";
+
+
+export const GABON_WIKI_EN = "https://en.wikipedia.org/wiki/Gabon";
+
+export const GABON_WIKI_FR = "https://fr.wikipedia.org/wiki/Gabon";
