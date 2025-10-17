f3804cb37a379ce781d7528483c17bb33ba97d0a
sergemezui <sergemezui.dev@gmail.com>
2025-10-17 19:47:41 +0100

    Refactor `Page` and `Layout` components to consolidate `Header` and `Social` into `Layout`, simplifying structure and ensuring consistent rendering.

--- app/[locale]/(site)/layout.tsx
+++ app/[locale]/(site)/layout.tsx
@@ -1,6 +1,7 @@
-import { AppProvider } from "@/context";
-import { ReactNode } from "react";
-import { setStaticParamsLocale } from "next-international/server";
+import { AppProvider } from '@/context';
+import { ReactNode } from 'react';
+import { setStaticParamsLocale } from 'next-international/server';
+import { Header, Social } from '@/components/molecule';
 
 export default async function Layout({ params, children }: {
   params: Promise<{ locale: string }>,
@@ -8,5 +9,10 @@ export default async function Layout({ params, children }: {
 }) {
   const { locale } = await params;
   setStaticParamsLocale(locale);
-  return <AppProvider locale={locale}>{children}</AppProvider>;
+  return <AppProvider locale={locale}>
+    <div className="min-h-screen">
+      <Header />{children}
+    </div>
+    <Social />
+  </AppProvider>;
 }
--- app/[locale]/(site)/page.tsx
+++ app/[locale]/(site)/page.tsx
@@ -6,29 +6,22 @@ import {
   ProjectCard,
   ServiceCard,
   SkillCard,
-} from "@/components/pages";
-import { Header, Social, Footer } from "@/components/molecule";
+} from '@/components/pages';
+import { Footer } from '@/components/molecule';
 
-import { setStaticParamsLocale } from "next-international/server";
+import { setStaticParamsLocale } from 'next-international/server';
 
-export default async function Home({ params }: { params: Promise<{ locale: "en" | "fr" }> }) {
+export default async function Home({ params }: { params: Promise<{ locale: 'en' | 'fr' }> }) {
   const { locale } = await params;
   setStaticParamsLocale(locale);
-  return (
-    <>
-      <div className="min-h-screen">
-        <Header/>
-        <div className="">
-          <HomeCard/>
-          <ServiceCard/>
-          <SkillCard/>
-          <ProjectCard/>
-          <EnterpriseCard/>
-          <ContactCard/>
-          <Footer/>
-        </div>
-      </div>
-      <Social/>
-    </>
+  return (<div className="">
+      <HomeCard />
+      <ServiceCard />
+      <SkillCard />
+      <ProjectCard />
+      <EnterpriseCard />
+      <ContactCard />
+      <Footer />
+    </div>
   );
 }
